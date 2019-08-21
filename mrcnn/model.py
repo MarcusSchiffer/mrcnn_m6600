f05ac12beda5dd3d9cde751b5d8697e0e3692cba
MarcusSchiffer <marcus.schiffer@evonik.com>
2019-08-21 18:43:59 +0200

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2359,8 +2359,9 @@ class MaskRCNN():
         if os.name is 'nt':
             workers = 0
         else:
-            workers = multiprocessing.cpu_count()
-
+	# Marcus für DATAHUB	
+        #    workers = multiprocessing.cpu_count()
+            workers = 0
         self.keras_model.fit_generator(
             train_generator,
             initial_epoch=self.epoch,
